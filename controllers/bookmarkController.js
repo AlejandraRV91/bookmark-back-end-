9d38f5075a1df7f7afe5a337903a266602dd838d
AlejandraRV91 <alejandraramirez@pursuit.org>
2023-07-25 15:04:23 -0500

    update and delete complete

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const bookmarks = express.Router();
-const { getAllBookmarks } = require("../queries/bookmarks");
+const {
+    getAllBookmarks,
+    getBookmark,
+    createBookmark,
+    deleteBookmark,
+    updateBookmark,
+} = require("../queries/bookmarks");
+const { checkBoolean, checkName, validateURL } = require("../validations/checkBookmarks.js");
+
 
 // INDEX
 bookmarks.get("/", async (req, res) => {
@@ -10,6 +18,45 @@ bookmarks.get("/", async (req, res) => {
     } else {
         res.status(500).json({ error: "server error" });
     }
+});
+
+// SHOW
+bookmarks.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const bookmark = await getBookmark(id);
+    if (bookmark) {
+        res.json(bookmark);
+    } else {
+        res.status(404).json({ error: "not found" });
+    }
+});
+
+// CREATE
+bookmarks.post("/", checkBoolean, checkName, validateURL, async (req, res) => {
+    try {
+        const bookmark = await createBookmark(req.body);
+        res.json(bookmark);
+    } catch (error) {
+        res.status(400).json({ error: error });
+    }
+});
+
+// DELETE
+bookmarks.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedBookmark = await deleteBookmark(id);
+    if (deletedBookmark.id) {
+        res.status(200).json(deletedBookmark);
+    } else {
+        res.status(404).json("Bookmark not found");
+    }
+});
+
+// UPDATE
+bookmarks.put("/:id", checkName, checkBoolean, validateURL, async (req, res) => {
+    const { id } = req.params;
+    const updatedBookmark = await updateBookmark(id, req.body);
+    res.status(200).json(updatedBookmark);
 });
 
 module.exports = bookmarks;
---
+++ validations/checkBookmarks.js
@@ -0,0 +1,30 @@
+const checkName = (req, res, next) => {
+    if (req.body.name) {
+        next();
+    } else {
+        res.status(400).json({ error: "Name is required" });
+    }
+};
+
+const checkBoolean = (req, res, next) => {
+    if (req.body.is_favorite) {
+        next();
+    } else {
+        res.status(400).json({ error: "is_favorite must be a boolean value" });
+    }
+};
+
+const validateURL = (req, res, next) => {
+    if (
+        req.body.url.substring(0, 7) === "http://" ||
+        req.body.url.substring(0, 8) === "https://"
+    ) {
+        return next();
+    } else {
+        res
+            .status(400)
+            .json({ error: `You forgot to start your url with http:// or https://` });
+    }
+};
+
+module.exports = { checkBoolean, checkName, validateURL };
